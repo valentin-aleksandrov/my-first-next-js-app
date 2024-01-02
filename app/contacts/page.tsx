58fe98c3fc7f0f720f8b230fbee9a7d1737f6899
Valentin Aleksandrov <valentin805@gmail.com>
2024-01-02 19:32:47 +0200

    implement navigation between the contacts page and dashboard page via the Link component from "next/link"

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Dashboard page
-      <Link href="/contacts">Contacts</Link>
+      Contacts page
+      <Link href="/dashboard">Dashboard</Link>
     </main>
   );
 }
