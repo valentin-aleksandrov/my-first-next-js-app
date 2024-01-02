16a15c75db80cf637b6e4020dee4db4320686643
Valentin Aleksandrov <valentin805@gmail.com>
2024-01-02 20:03:00 +0200

    add an example of next.js navigation using the useRouter hook

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,12 +1,18 @@
+"use client";
 import Link from "next/link";
 import { Links } from "../components/links";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Links />
       Contacts page
       <Link href="/dashboard">Dashboard</Link>
+      <button type="button" onClick={() => router.push("/dashboard")}>
+        Go to Dashboard via the NextJS Router
+      </button>
     </main>
   );
 }
