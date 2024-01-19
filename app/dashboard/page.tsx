7e809c1482c47b0c7485a326a75af8d28ab54a94
Valentin Aleksandrov <valentin805@gmail.com>
2024-01-19 20:38:48 +0200

    Add an example that replacing the current entry on the browser's history stack.
    - The user is not able to navigate back to the previous state.
    - For example, to switch the application's locale:

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,24 @@
 "use client";
 import Link from "next/link";
 import { Links } from "../components/links";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, usePathname } from "next/navigation";
+
+export function LocaleSwitcher() {
+  const pathname = usePathname();
+
+  function switchLocale(locale: string) {
+    // e.g. '/en/about' or '/fr/contact'
+    const newPath = `/${locale}${pathname}`;
+    window.history.replaceState(null, "", newPath);
+  }
+
+  return (
+    <>
+      <button onClick={() => switchLocale("en")}>English</button>
+      <button onClick={() => switchLocale("fr")}>French</button>
+    </>
+  );
+}
 
 function SortProducts() {
   const searchParams = useSearchParams();
@@ -27,6 +44,7 @@ export default function Page() {
       Dashboard page
       <Link href="/contacts">Contacts</Link>
       <SortProducts />
+      <LocaleSwitcher />
     </main>
   );
 }
